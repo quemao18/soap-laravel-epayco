f35c37b6c080bf488e308794e1087da5e74b31cd
DominicTalbot <dominic@royand.co>
2015-12-14 08:28:16 +0000

    Sort out Seeders File

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,13 +16,6 @@ class DatabaseSeeder extends Seeder
 
         $this->call('ApiKeyTableSeeder');
         $this->call('UserTableSeeder');
-        $this->call('SiteTableSeeder');
-        $this->call('ServiceTableSeeder');
-        $this->call('PackageTableSeeder');
-        $this->call('QuoteTableSeeder');
-        $this->call('PageTableSeeder');
-        $this->call('EnquiryTableSeeder');
-        $this->call('DocumentTableSeeder');
 
         Model::reguard();
     }
